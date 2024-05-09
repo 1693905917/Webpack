fec37627f3065e211e5e6487cc944201b00afc3e
1693905917 <1693905917@qq.com>
2024-05-09 22:25:17 +0800

    Webpack_Study2.0

--- 03_Webpack_Study/src/login/index.js
+++ 03_Webpack_Study/src/login/index.js
@@ -15,22 +15,22 @@ console.log(checkCode('123123123123'))
  *  3.3 打包并手动复制网页到 dist 下，引入打包后的 js，运行
  */
 // 3.2 编写核心 JS 逻辑代码
-document.querySelector('.btn').addEventListener('click', () => {
-    const phone = document.querySelector('.login-form [name=mobile]').value
-    const code = document.querySelector('.login-form [name=code]').value
+// document.querySelector('.btn').addEventListener('click', () => {
+//     const phone = document.querySelector('.login-form [name=mobile]').value
+//     const code = document.querySelector('.login-form [name=code]').value
   
-    if (!checkPhone(phone)) {
-      console.log('手机号长度必须是11位')
-      return
-    }
+//     if (!checkPhone(phone)) {
+//       console.log('手机号长度必须是11位')
+//       return
+//     }
   
-    if (!checkCode(code)) {
-      console.log('验证码长度必须是6位')
-      return
-    }
+//     if (!checkCode(code)) {
+//       console.log('验证码长度必须是6位')
+//       return
+//     }
   
-    console.log('提交到服务器登录...')
-  })
+//     console.log('提交到服务器登录...')
+//   })
 /**
  * 目标5：打包 css 代码
  *  5.1 准备 css 代码，并引入到 js 中
@@ -42,4 +42,74 @@ document.querySelector('.btn').addEventListener('click', () => {
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import './index.less'
+/**
+ * 目标9：打包资源模块（图片处理）
+ *  9.1 创建 img 标签并动态添加到页面，配置 webpack.config.js
+ *  9.2 打包后观察效果和区别
+ */
+// 9.1 创建 img 标签并动态添加到页面，配置 webpack.config.js
+// 注意：js 中引入本地图片资源要用 import 方式（如果是网络图片http地址，字符串可以直接写）
+import imgObj from './assets/logo.png'
+const theImg = document.createElement('img')
+theImg.src = imgObj
+document.querySelector('.login-wrap').appendChild(theImg)
+
+
+/**
+ * 目标10：完成登录功能
+ *  10.1 使用 npm 下载 axios（体验 npm 作用在前端项目中）
+ *  10.2 准备并修改 utils 工具包源代码导出实现函数
+ *  10.3 导入并编写逻辑代码，打包后运行观察效果
+ */
+// 10.3 导入并编写逻辑代码，打包后运行观察效果
+import myAxios from '../utils/request.js'
+import { myAlert } from '../utils/alert.js'
+document.querySelector('.btn').addEventListener('click', () => {
+  const phone = document.querySelector('.login-form [name=mobile]').value
+  const code = document.querySelector('.login-form [name=code]').value
+
+  if (!checkPhone(phone)) {
+    myAlert(false, '手机号长度必须是11位')
+    console.log('手机号长度必须是11位')
+    return
+  }
+
+  if (!checkCode(code)) {
+    myAlert(false, '验证码长度必须是6位')
+    console.log('验证码长度必须是6位')
+    return
+  }
+
+  myAxios({
+    url: '/v1_0/authorizations',
+    method: 'POST',
+    data: {
+      mobile: phone,
+      code: code
+    }
+  }).then(res => {
+    myAlert(true, '登录成功')
+    // localStorage.setItem('token', res.data.token)
+    // location.href = '../content/index.html'
+  }).catch(error => {
+    myAlert(false, error.response.data.message)
+  })
+})
+
+
+//注意1：webpack-dev-server借助http模块创建8080默认Web服务
+//注意2：默认以当前项目根目录的pub1ic文件夹的index.html作为服务器根目录
+//注意3：webpack-dev-server根据配置，打包相关代码在内存当中，以output.path的值作为服务器根目录（所以可以直接自己拼接访间dist目录下内容）
+console.log("自动打包")
+
+
+if(process.env.NODE_ENV === 'production'){
+  //就让打印语句失效
+  console.log=function(){}
+}
+console.log("开发模式下好用，生产模式下失效")
+
+//错误
+// consolee.warning("123")
+
 
--- 03_Webpack_Study/webpack.config.js
+++ 03_Webpack_Study/webpack.config.js
@@ -4,7 +4,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
-module.exports = {
+const webpack = require('webpack')
+
+//用一个常量config来判断
+const config={
+  // mode: 'development',
   //entry：入口修改处  __dirname:获取当前文件所在的文件夹的绝对路径 D:\BaiduNetdiskDownload\VSCode\VSCodeFile\03_Webpack_Study\src/login/index.js
   entry: path.resolve(__dirname, 'src/login/index.js'),
   //output:出口修改处
@@ -19,20 +23,44 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname,'public/login.html'), // 模板文件
-      filename: path.resolve(__dirname,'dist/login/index.html') // 输出文件
+      filename: path.resolve(__dirname,'dist/login/index.html'),// 输出文件
+      // 自定义属性，在 html 模板中 <%=htmlWebpackPlugin.options.useCdn%> 访问使用
+      useCdn: process.env.NODE_ENV === 'production'
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin({
+      //在MiniCssExtractPlugin插件是不能用绝对路径的，只能用相对路径
+      filename:'./login/index.css'
+    }),
+    new webpack.DefinePlugin({
+      // key 是注入到打包后的前端 JS 代码中作为全局变量
+      // value 是变量对应的值（在 corss-env 注入在 node.js 中的环境变量字符串）
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ],
   module: { // 加载器
     rules: [ // 规则列表
       {
         test: /\.css$/i, // 匹配 .css 结尾的文件
         //use: ['style-loader', 'css-loader'], // 使用从后到前的加载器来解析 css 代码和插入到 DOM  不要颠倒顺序
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        //use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.less$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+       // use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+       use: [
+        // compiles Less to CSS
+        process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader',
+        'less-loader',
+      ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'assets/[hash][ext][query]'
+        }
       }
     ]
   },
@@ -45,7 +73,29 @@ module.exports = {
       `...`,
       new CssMinimizerPlugin(),
     ],
+  },
+  //解析
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 }
+//开发环境下使用source-map
+if(process.env.NODE_ENV === 'development'){
+  config.devtool='inline-source-map'
+}
+
+// 生产环境下使用相关配置
+if (process.env.NODE_ENV === 'production') {
+  // 外部扩展（让 webpack 防止 import 的包被打包进来）
+  config.externals = {
+    // key：import from 语句后面的字符串
+    // value：留在原地的全局变量（最好和 cdn 在全局暴露的变量一致）
+    'bootstrap/dist/css/bootstrap.min.css': 'bootstrap',
+    'axios': 'axios'
+  }
+}
+module.exports = config
 
 
